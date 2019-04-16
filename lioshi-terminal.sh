ccd23d86f60673cd5c43853ce9fd5c3194ee811a
lioshi <lionel.fenneteau@gmail.com>
2019-04-16 10:33:11 +0200

    new colors order

--- lioshi-terminal.sh
+++ lioshi-terminal.sh
@@ -10,7 +10,7 @@ dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/font "Hack 12"
 dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/use-theme-transparency "true"
 dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/foreground-color "'rgb(176,176,176)'"
 dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/background-color "'rgb(48,48,48)'"
-dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/palette "['rgb(48, 48, 48)','rgb(181, 189, 104)','rgb(204, 102, 102)','rgb(240, 198, 116)','rgb(129, 162, 190)','rgb(93, 150, 141)','rgb(190, 148, 187)','rgb(197, 200, 198)','rgb(86, 86, 86)','rgb(207, 212, 157)','rgb(223, 159, 159)','rgb(247, 226, 185)','rgb(179, 199, 216)','rgb(137, 182, 175)','rgb(219, 196, 217)','rgb(236, 237, 237)']"
+dconf write /org/gnome/terminal/legacy/profiles:/:$trpr/palette "['rgb(48, 48, 48)','rgb(204, 102, 102)','rgb(181, 189, 104)','rgb(240, 198, 116)','rgb(129, 162, 190)','rgb(190, 148, 187)','rgb(93, 150, 141)','rgb(197, 200, 198)','rgb(86, 86, 86)','rgb(223, 159, 159)','rgb(207, 212, 157)','rgb(247, 226, 185)','rgb(179, 199, 216)','rgb(219, 196, 217)','rgb(137, 182, 175)','rgb(236, 237, 237)']"
 
 
 
